759ed97540fc6bd1ab0c7d370c6b3e65f659e5f3
Violet Voronetzky <sigalvo@microsoft.com>
2020-02-04 12:34:43 +0200

    Fixed typo

--- src/visualizers/highcharts/common/utilities.ts
+++ src/visualizers/highcharts/common/utilities.ts
@@ -7,7 +7,7 @@ export class HC_Utilities {
         const originalValue = row[yAxisIndex];
         const column = columns[yAxisIndex];
 
-        // Highcharts support only numeric y-axis data - if the y-axis isn't a number (can be a string that represents a number "0.005" for example) - convert it to number
+        // Highcharts support only numeric y-axis data. If the y-axis isn't a number (can be a string that represents a number "0.005" for example) - convert it to number
         if(typeof originalValue === 'string') {
             if(column.type === DraftColumnType.Decimal) {
                 return parseFloat(originalValue);
