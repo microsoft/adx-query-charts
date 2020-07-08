f012094c7b39c8e665911fb00c6cc112afeb15e8
Violet Voronetzky <sigalvo@microsoft.com>
2020-07-08 11:05:39 +0300

    Fixed a bug when y-axis is zero

--- src/visualizers/highcharts/common/utilities.ts
+++ src/visualizers/highcharts/common/utilities.ts
@@ -4,8 +4,13 @@ import { DraftColumnType, IColumn, IRow } from "../../../common/chartModels";
 
 export class HC_Utilities {
     public static getYValue(columns: IColumn[], row: IRow, yAxisIndex: number): number {
-        const originalValue = row[yAxisIndex] || null; // If the y value is undefined - convert to null since Highcharts don't support numeric undefined values
         const column = columns[yAxisIndex];
+        let originalValue = row[yAxisIndex]; 
+        
+        // If the y value is undefined - convert to null since Highcharts don't support numeric undefined values
+        if(originalValue === undefined) {
+            originalValue = null;
+        }
 
         // Highcharts support only numeric y-axis data. If the y-axis isn't a number (can be a string that represents a number "0.005" for example) - convert it to number
         if(typeof originalValue === 'string') {
