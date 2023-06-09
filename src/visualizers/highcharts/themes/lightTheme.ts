0816c1a94b472be6ec7d08d52ef1b406af571be2
kbostoen <kbostoen@inimco.com>
2023-06-09 09:39:18 +0200

    add more colors

--- src/visualizers/highcharts/themes/lightTheme.ts
+++ src/visualizers/highcharts/themes/lightTheme.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 export const LightThemeOptions: Highcharts.Options = {
-    colors: ['#0078D4', '#EF6950', '#00188F', '#00A2AD', '#4B003F', '#E3008C', '#022F22', '#917EDB', '#001D3F', '#502006'],
+    colors: ['#0078D4', '#EF6950', '#00188F', '#00A2AD', '#4B003F', '#E3008C', '#022F22', '#917EDB', '#001D3F', '#502006','#6A6B07', '#FFAF00', '#008080', '#AB5A92', '#1388B8', '#B53471', '#007A4D', '#9D8CBE', '#00385A', '#68291D'],
     tooltip: {
         backgroundColor: 'rgba(255, 255, 255, 0.85)'
     },
